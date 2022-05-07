6f28c681ff06bff46df5726cbf614ad02ea81daf
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-05-07 17:29:21 -0500

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -12,8 +12,6 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.addons.ui.FlxUIState;
 import flixel.math.FlxRect;
 import flixel.util.FlxTimer;
-#if mobileC
-import mobile.FlxVirtualPad;
 import flixel.input.actions.FlxActionInput;
 #end
 
